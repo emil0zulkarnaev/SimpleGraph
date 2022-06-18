f226904445727fa93d2558760b5b9feafacc629e
emil zulkarnaev <emil0zulkarnaev@gmail.com>
2022-06-18 09:04:59 +0500

    Исправил обновление данных на графике

--- SimpleGraph.js
+++ SimpleGraph.js
@@ -9,8 +9,9 @@ class SimpleGraph {
 		this.svg.style.fontSize = "13px";
 		this.fontSize = 13;
 		this.grid = false;
-		this.data = []; // [x,y]
+		this.data = [];
 		this.data_lines = [];
+		this.grid_elements = [];
 		this.padding = 50;
 		container.appendChild(this.svg);
 
@@ -45,7 +46,11 @@ class SimpleGraph {
 			for (let el of row) el.remove();
 		}
 
-		let grid_elements = [];
+		this.data_lines = [];
+
+		for (let el of this.grid_elements) el.remove();
+
+		this.grid_elements = [];
 
 		for (let j=0; j<this.data.length; j++) {
 			this.data_lines.push([]);
@@ -60,6 +65,7 @@ class SimpleGraph {
 			}
 			let denominatorx = maxx-minx,
 				denominatory = maxy-miny;
+
 			for (let i=1; i<row.length; i++) {
 				let line = document.createElementNS(this.xmlns, "line");
 				line.setAttributeNS(null, "x1", this.padding+(this.params.width-this.padding*2)*(row[i-1][0]-minx)/denominatorx);
@@ -81,7 +87,7 @@ class SimpleGraph {
 				grid_line.setAttributeNS(null, "y2", this.params.height-(this.padding+(this.params.height-this.padding*2)*(row[i][1]-miny)/denominatory));
 				grid_line.setAttributeNS(null, "stroke", "rgba(0,0,0,.2)");
 
-				grid_elements.push(grid_line);
+				this.grid_elements.push(grid_line);
 
 				grid_line = document.createElementNS(this.xmlns, "line");
 				grid_line.setAttributeNS(null, "x1", this.padding+(this.params.width-this.padding*2)*(row[i][0]-minx)/denominatorx);
@@ -90,7 +96,7 @@ class SimpleGraph {
 				grid_line.setAttributeNS(null, "y2", this.params.height-(this.padding-10));
 				grid_line.setAttributeNS(null, "stroke", "rgba(0,0,0,.2)");
 
-				grid_elements.push(grid_line);
+				this.grid_elements.push(grid_line);
 
 				let content = String(row[i][1]);
 				let text = document.createElementNS(this.xmlns, "text");
@@ -98,7 +104,7 @@ class SimpleGraph {
 				text.setAttributeNS(null, 'x', this.padding-content.length*this.fontSize);
 				text.setAttributeNS(null, 'y', this.params.height-(this.padding+(this.params.height-this.padding*2)*(row[i][1]-miny)/denominatory));
 
-				grid_elements.push(text);
+				this.grid_elements.push(text);
 
 				content = String(row[i][0]);
 				text = document.createElementNS(this.xmlns, "text");
@@ -106,14 +112,14 @@ class SimpleGraph {
 				text.setAttributeNS(null, 'x', this.padding+(this.params.width-this.padding*2)*(row[i][0]-minx)/denominatorx);
 				text.setAttributeNS(null, 'y', this.params.height-(this.padding-this.fontSize));
 
-				grid_elements.push(text);
+				this.grid_elements.push(text);
 
 				this.data_lines[j].push(line);
 				this.data_lines[j].push(circle);
 			}
 		}
 
-		for (let el of grid_elements) this.svg.appendChild(el);
+		for (let el of this.grid_elements) this.svg.appendChild(el);
 		for (let row of this.data_lines) {
 			for (let el of row) this.svg.appendChild(el);
 		}
